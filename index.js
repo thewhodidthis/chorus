0bf0ed291d28acc2b4eada21c027941fb3b3c394
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-09-27 09:42:32 +0200

    module: add optional target value on release

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var createTrigger = function (audio, param) {
   var play = function (target, attack, decay, sustain) {
     if ( target === void 0 ) target = 1;
-    if ( attack === void 0 ) attack = 0.5;
+    if ( attack === void 0 ) attack = 0.25;
     if ( decay === void 0 ) decay = 0.5;
     if ( sustain === void 0 ) sustain = 1;
 
@@ -18,14 +18,15 @@ var createTrigger = function (audio, param) {
     return true
   };
 
-  var stop = function (release) {
+  var stop = function (release, target) {
     if ( release === void 0 ) release = 0.5;
+    if ( target === void 0 ) target = 0;
 
     var time = audio.currentTime;
 
     param.cancelScheduledValues(0);
     param.setValueAtTime(param.value, time);
-    param.linearRampToValueAtTime(0, time + release);
+    param.linearRampToValueAtTime(target, time + release);
 
     return false
   };
@@ -33,7 +34,11 @@ var createTrigger = function (audio, param) {
   var busy = false;
 
   return function next() {
+    // Not particularly fond of using `arguments`,
+    // but for now save on the while loop traspiling rest params will introduce
     busy = (busy ? stop : play).apply(null, arguments);
+
+    return busy
   }
 };
 
--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 const createTrigger = (audio, param) => {
-  const play = (target = 1, attack = 0.5, decay = 0.5, sustain = 1) => {
+  const play = (target = 1, attack = 0.25, decay = 0.5, sustain = 1) => {
     const time = audio.currentTime
 
     param.cancelScheduledValues(time)
@@ -11,12 +11,12 @@ const createTrigger = (audio, param) => {
     return true
   }
 
-  const stop = (release = 0.5) => {
+  const stop = (release = 0.5, target = 0) => {
     const time = audio.currentTime
 
     param.cancelScheduledValues(0)
     param.setValueAtTime(param.value, time)
-    param.linearRampToValueAtTime(0, time + release)
+    param.linearRampToValueAtTime(target, time + release)
 
     return false
   }
@@ -24,7 +24,11 @@ const createTrigger = (audio, param) => {
   let busy = false
 
   return function next() {
+    // Not particularly fond of using `arguments`,
+    // but for now save on the while loop traspiling rest params will introduce
     busy = (busy ? stop : play).apply(null, arguments)
+
+    return busy
   }
 }
 
