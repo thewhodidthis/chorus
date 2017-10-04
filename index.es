10ee10305023e0cd3d6aff793b5d0d63d3fb83f6
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-10-04 23:52:59 +0200

    module: cleanup, rework argument handling, buble not

---
+++ index.es
@@ -0,0 +1,35 @@
+const trigger = ({ context = new AudioContext(), param = context.createGain() } = {}) => {
+  const trip = (param instanceof AudioParam && param) || param.gain
+
+  const play = ({ target = 1, attack = 0.25, decay = 0.5, sustain = 1 } = {}) => {
+    const { currentTime } = context
+
+    trip.cancelScheduledValues(currentTime)
+    trip.value = target
+    trip.setValueAtTime(0, currentTime)
+    trip.linearRampToValueAtTime(target, currentTime + attack)
+    trip.linearRampToValueAtTime(target * sustain, currentTime + attack + decay)
+
+    return true
+  }
+
+  const stop = ({ target = 0, release = 0.5 } = {}) => {
+    const { currentTime } = context
+
+    trip.cancelScheduledValues(0)
+    trip.setValueAtTime(trip.value, currentTime)
+    trip.linearRampToValueAtTime(target, currentTime + release)
+
+    return false
+  }
+
+  let isBusy = false
+
+  return (o) => {
+    isBusy = isBusy ? stop(o) : play(o)
+
+    return isBusy
+  }
+}
+
+export default trigger
--- index.js
+++ index.js
@@ -1,46 +1,37 @@
 'use strict';
 
-var createTrigger = function (audio, param) {
-  var play = function (target, attack, decay, sustain) {
-    if ( target === void 0 ) target = 1;
-    if ( attack === void 0 ) attack = 0.25;
-    if ( decay === void 0 ) decay = 0.5;
-    if ( sustain === void 0 ) sustain = 1;
+const trigger = ({ context = new AudioContext(), param = context.createGain() } = {}) => {
+  const trip = (param instanceof AudioParam && param) || param.gain;
 
-    var time = audio.currentTime;
+  const play = ({ target = 1, attack = 0.25, decay = 0.5, sustain = 1 } = {}) => {
+    const { currentTime } = context;
 
-    param.cancelScheduledValues(time);
-    param.value = target;
-    param.setValueAtTime(0, time);
-    param.linearRampToValueAtTime(target, time + attack);
-    param.linearRampToValueAtTime(target * sustain, time + attack + decay);
+    trip.cancelScheduledValues(currentTime);
+    trip.value = target;
+    trip.setValueAtTime(0, currentTime);
+    trip.linearRampToValueAtTime(target, currentTime + attack);
+    trip.linearRampToValueAtTime(target * sustain, currentTime + attack + decay);
 
     return true
   };
 
-  var stop = function (release, target) {
-    if ( release === void 0 ) release = 0.5;
-    if ( target === void 0 ) target = 0;
+  const stop = ({ target = 0, release = 0.5 } = {}) => {
+    const { currentTime } = context;
 
-    var time = audio.currentTime;
-
-    param.cancelScheduledValues(0);
-    param.setValueAtTime(param.value, time);
-    param.linearRampToValueAtTime(target, time + release);
+    trip.cancelScheduledValues(0);
+    trip.setValueAtTime(trip.value, currentTime);
+    trip.linearRampToValueAtTime(target, currentTime + release);
 
     return false
   };
 
-  var busy = false;
+  let isBusy = false;
 
-  return function next() {
-    // Not particularly fond of using `arguments`,
-    // but for now save on the while loop traspiling rest params will introduce
-    busy = (busy ? stop : play).apply(null, arguments);
+  return (o) => {
+    isBusy = isBusy ? stop(o) : play(o);
 
-    return busy
+    return isBusy
   }
 };
 
-module.exports = createTrigger;
-
+module.exports = trigger;
