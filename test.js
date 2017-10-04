af45261e2e4715cb34bfc1732312567347ac04c1
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-10-04 23:53:15 +0200

    test: expand

--- test.js
+++ test.js
@@ -4,9 +4,27 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext
 
 const kpow = require('kpow')
 const test = require('tape')
+const trigger = require('./')
 
 kpow()
 
-test('will init', (t) => {
+test('will default', (t) => {
+  const toggle = trigger(undefined, {})
+
+  t.equals(typeof toggle, 'function', 'got lambda on init')
+  t.end()
+})
+
+test('will toggle', (t) => {
+  const toggle = trigger()
+
+  let isOn = false
+
+  isOn = toggle()
+  t.ok(isOn, 'is on')
+
+  isOn = toggle()
+  t.notOk(isOn, 'is off')
+
   t.end()
 })
