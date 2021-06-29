6f90f11039c90e9a42b7ea398d445d723dee0305
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-06-29 21:14:23 +0100

    test: fix imports, use double quotes

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-import 'cutaway'
-import { assert, report } from 'tapeless'
-import trigger from './index.mjs'
+import "cutaway"
+import { assert, report } from "tapeless"
+import trigger from "./main.js"
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext
 
@@ -9,17 +9,17 @@ const { ok, notOk, equal } = assert
 const t1 = trigger(undefined, {})
 
 equal
-  .describe('got lambda on init', 'will default')
-  .test(typeof t1, 'function')
+  .describe("got lambda on init", "will default")
+  .test(typeof t1, "function")
 
 const t2 = trigger()
 
 ok
   .test(t2)
-  .test(t2(), 'is on')
+  .test(t2(), "is on")
 
 notOk
-  .describe(null, 'will toggle')
-  .test(t2(), 'is off')
+  .describe(null, "will toggle")
+  .test(t2(), "is off")
 
 report()
